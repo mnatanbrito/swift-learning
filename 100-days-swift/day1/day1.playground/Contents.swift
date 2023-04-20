5789ff2ee863de98e86ed8223daf5a32806d9063
mnatanbrito <mnatan.brito@gmail.com>
2023-04-20 01:23:38 -0400

    adds files for day 1

---
+++ 100-days-swift/day1/day1.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+// Variables
+// Variables use var
+// Contants use let
+
+// string and integers
+var greeting = "Hello, playground"
+var age = 38
+var population = 8_000_000
+
+// multi-line strings
+var str1 = """
+this goes
+over multiple
+lines
+"""
+
+// Doubles and booleans
+var pi = 3.141
+
+// String interpolation
+var score = 85
+var str2 = "Your score was \(score)"
+var results = "The test results are here: \(str2)"
+
+// Constants
+let taylor = "swift"
+
+// Type annotations
+let str3 = "Hello, playground"
+let str4: String = "Reputation"
